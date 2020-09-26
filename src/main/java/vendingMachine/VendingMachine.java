35ad3a95dc0c2eac3411cae837c005a58542b99a
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-09-26 16:00:30 +0100

    can get inventory and money paid passes

--- src/main/java/vendingMachine/VendingMachine.java
+++ src/main/java/vendingMachine/VendingMachine.java
@@ -1,4 +1,36 @@
 package vendingMachine;
 
+import items.SnackType;
+
+import java.util.ArrayList;
+
 public class VendingMachine {
+
+    private int moneyPaid;
+    private ArrayList<SnackType> inventory;
+
+    public VendingMachine() {
+        this.moneyPaid = 0;
+        this.inventory = new ArrayList<SnackType>();
+
+    }
+
+    public int getMoneyPaid() {
+        return moneyPaid;
+    }
+
+    public int getInventory(){
+        return inventory.size();
+    }
+
+    public void setMoneyPaid(int moneyPaid) {
+        this.moneyPaid = moneyPaid;
+    }
+    public void setInventory(ArrayList<SnackType> inventory){
+        this.inventory = inventory;
+    }
+
+    addProductToInventory(SnackType snack){
+        this.inventory.add(snack);
+    }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,2 +1,30 @@
+import items.CoinType;
+import items.SnackType;
+import org.junit.Before;
+import org.junit.Test;
+import vendingMachine.VendingMachine;
+
+import static org.junit.Assert.assertEquals;
+
 public class VendingMachineTest {
+
+    SnackType sweets;
+    VendingMachine vendingMachine;
+    SnackType snackType;
+    CoinType coinType;
+
+    @Before
+    public void before(){
+        vendingMachine = new VendingMachine();
+    }
+    @Test
+    public void canGetInventory(){
+        assertEquals(0, vendingMachine.getInventory());
+    }
+    @Test
+    public void canGetMoneyPaid(){
+        assertEquals(0, vendingMachine.getMoneyPaid(), 0.5);
+    }
+
+
 }
